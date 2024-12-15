5d6224117a0ded57d3722192b268f44ba796e061
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-12-15 18:16:39 +0100

    first commit

---
+++ src/main/java/com/taskSync/TaskSync_backend/TaskSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskSync.TaskSync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSyncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskSync/TaskSync_backend/TaskSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskSync.TaskSync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
