d433bc89589a073e3a80bb991b6fae3b9039378f
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-12-17 18:44:08 +0100

    Task => Delete, Put and Get Method

--- src/main/java/com/taskSync/TaskSync_backend/controller/TaskController.java
+++ src/main/java/com/taskSync/TaskSync_backend/controller/TaskController.java
@@ -2,10 +2,9 @@ package com.taskSync.TaskSync_backend.controller;
 
 import com.taskSync.TaskSync_backend.entity.Task;
 import com.taskSync.TaskSync_backend.service.TaskService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -22,4 +21,20 @@ public class TaskController {
     public void createTask(@RequestBody Task task){
         this.taskService.createTask(task);
     }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public List<Task> getListTask(){
+        return this.taskService.getListTask();
+    }
+
+    @PutMapping(path = "{id}", consumes = APPLICATION_JSON_VALUE)
+    public void modifyTask(@PathVariable int id, @RequestBody Task task){
+        this.taskService.modifyTask(id, task);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteTask(@PathVariable int id){
+        this.taskService.deleteTask(id);
+    }
+
 }
--- src/main/java/com/taskSync/TaskSync_backend/service/TaskService.java
+++ src/main/java/com/taskSync/TaskSync_backend/service/TaskService.java
@@ -5,6 +5,9 @@ import com.taskSync.TaskSync_backend.entity.User;
 import com.taskSync.TaskSync_backend.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
     private TaskRepository taskRepository;
@@ -18,5 +21,29 @@ public class TaskService {
         User user = this.userService.getUser(task.getUser());
         task.setUser(user);
         this.taskRepository.save(task);
+    }
+
+    public List<Task> getListTask() {
+        return this.taskRepository.findAll();
+    }
+
+    public void modifyTask(int id, Task task) {
+        Optional<Task> optionalTask = this.taskRepository.findById(id);
+        if (optionalTask.isPresent()){
+            Task modifyTask = optionalTask.get();
+            modifyTask.setTitle(task.getTitle());
+            modifyTask.setDescription(task.getDescription());
+            modifyTask.setDueDate(task.getDueDate());
+            modifyTask.setTags(task.getTags());
+            modifyTask.setStatus(task.getStatus());
+            this.taskRepository.save(modifyTask);
+        }
+        else {
+            throw new RuntimeException("Client with ID " + id + " not found.");
+        }
+    }
+
+    public void deleteTask(int id) {
+        this.taskRepository.deleteById(id);
     }
 }
