ee7c3a74c697be960d2497a1d7a45c97d9159a55
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-12-19 16:26:40 +0100

    Add method Authentification

--- src/main/java/com/taskSync/TaskSync_backend/security/ConfigSecurityApp.java
+++ src/main/java/com/taskSync/TaskSync_backend/security/ConfigSecurityApp.java
@@ -1,16 +1,23 @@
 package com.taskSync.TaskSync_backend.security;
 
+import com.taskSync.TaskSync_backend.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ConfigSecurityApp {
+public class ConfigSecurityApp{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -21,6 +28,7 @@ public class ConfigSecurityApp {
                                 authorize ->
                                         authorize
                                                 .requestMatchers("/user").permitAll()
+                                                .requestMatchers("/user/connection").permitAll()
                                                 .requestMatchers("/tasks").permitAll()
                                                 .anyRequest().authenticated()
                         )
@@ -31,4 +39,16 @@ public class ConfigSecurityApp {
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService userService){
+        return userService;
+    }
+
+
 }
